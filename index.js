c6042085803b61ea979d106293af235004e58cc1
Niamul Hasan Zead <zead.ece2014@gmail.com>
2022-05-08 23:25:47 +0600

    jwt is rechecked

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vqrww.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -58,13 +74,20 @@ async function run(){
 
                 })
                 // Api for loading all inventories to Bookings Component 
-                app.get('/bookings', async(req,res)=>{
+                app.get('/bookings',verifyJWT, async(req,res)=>{
+                    const authHeader=req.headers.authorization;
+                    console.log(authHeader);
                    const email=req.query.email;
-                   console.log(email)
-                   const query={email:email};
+                   if (email === decodedEmail) {
+                    const query={email:email};
                    const cursor=myCollection.find(query);
                    const bookings=await cursor.toArray();
                    res.send(bookings);
+                   }
+                   else{
+                    res.status(403).send({message: 'You have a forbidden access'})
+                }
+                   
                 })
 
                 //Api for inserting booking inventory
